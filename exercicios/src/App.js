5d2395ce978ff4cfc4e818c3fdc94996d57feeed
Gabriel Cardelli <cardelli@impa.br>
2024-09-15 12:11:39 -0300

    Finalizando aula 24 - Props

---
+++ exercicios/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Primeiro from "./components/Primeiro";
+import X, {Comp1,Comp2} from './components/Multi';
+import MinMax from "./components/MinMax";
+
+
+export default () => 
+    <View style={style.App}>
+        <MinMax min={3} max={20} />
+        <MinMax min={1} max={94} />
+        {/* <X />
+        <Primeiro/>
+        <Comp1 />
+        <Comp2 /> */}
+    </View>
+
+const  style = StyleSheet.create({
+    App: {
+        // backgroundColor: "#A00",
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
+    }
+});
---
+++ exercicios/src/components/MinMax.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { Text } from "react-native";
+import Estilo from "./estilo";
+export default (props) =>  <Text style={Estilo.txtG}>O valor {props.max} e maior que o valor {props.min}</Text>
